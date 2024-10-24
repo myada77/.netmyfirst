21de449a4d129bda2882cef0823e2f966f036d98
myada <mayadamoheyeldeen007@gmail.com>
2024-10-24 15:15:51 +0300

    Add Program.cs file

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AreaRectangular
+{
+    class Rec
+    {
+        double length;
+        double width;
+
+        public void Details()
+        {
+            length = 3;
+            width = 3.5;
+        }
+
+        public double Area()
+        {
+            return width * length;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", Area());
+        }
+    }
+
+    class Execute
+    {
+        static void Main(string[] args)
+        {
+            Rec r1 = new Rec();
+            r1.Details();
+            r1.Display();
+            Console.ReadLine(); // يبقي نافذة الكونسول مفتوحة
+        }
+    }
+}
